45286f55a0001fafd601c702cb5e3d2a51225819
Santiago Díaz <86180162+LazyFace@users.noreply.github.com>
2024-03-11 00:22:23 -0500

    Model player, new anim

--- Assets/Scripts/PlayerAnimationController.cs
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -9,12 +9,6 @@ public class PlayerAnimationController : MonoBehaviour
     private Animator animator;
     private string currentState;
 
-    //Animations
-    static readonly AnimationData idleAnimation = new AnimationData("idle", true);
-    static readonly AnimationData walkingAnimation = new AnimationData("walk", true);
-    static readonly AnimationData holdingRight = new AnimationData("holdingRight", false);
-    static readonly AnimationData rightShoot = new AnimationData("holdinRightShoot", false);
-
     //Audio
     [SerializeField] private AudioSource audioSource;
 
@@ -32,38 +26,15 @@ public class PlayerAnimationController : MonoBehaviour
         if (Mathf.Abs(rb.velocity.x) > speedThreshold || Mathf.Abs(rb.velocity.z) > speedThreshold)
         {
             audioSource.Play();
-            ChangeAnimation(walkingAnimation.animationName, idleAnimation.isLoop);
+            animator.SetBool("isWalking", true);
         }
         else
         {
             audioSource.Stop();
-            ChangeAnimation(idleAnimation.animationName, idleAnimation.isLoop);
+            animator.SetBool("isWalking", false);
         }
 
         //Debug.Log("Velocidad X  " + rb.velocity.x);
         //Debug.Log("Velocidad Z  " + rb.velocity.z);
-    }
-
-    private void ChangeAnimation(string newState, bool isLoop)
-    {
-        if (isLoop && newState == currentState)
-        {
-            return;
-        }
-        animator.Rebind();
-        currentState = newState;
-        animator.Play(newState);
-    }
-
-    private bool isAnimationPlaying(Animator animator, string animName)
-    {
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName(animName) && animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
     }
 }
