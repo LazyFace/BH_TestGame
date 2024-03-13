35436cae1b82429efa1e72059b95b5377fc23649
Sisabe999 <emmanuel.mv99@gmail.com>
2024-03-12 23:08:27 -0500

    Level loader Fixed

--- Assets/MonoTest/MScripts/LevelLoaderAuxiliar.cs
+++ Assets/MonoTest/MScripts/LevelLoaderAuxiliar.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class LevelLoaderAuxiliar : MonoBehaviour
 {
-    public void ChangeScene(int buildIndex)
-    { 
+    public void LoadScene(int buildIndex)
+    {
+        //Debug.Log("Si entro " + buildIndex);
         LevelLoader.instance.ChangeScene(buildIndex);
     }
 
